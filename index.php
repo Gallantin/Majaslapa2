8473555a2326f14f2e6cbb2d54b6320c227e5937
Gallantin <okoikwedced@gmail.com>
2020-11-26 17:43:59 +0200

    New

---
+++ index.php
@@ -0,0 +1,258 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Pizza</title>
+	<link rel="stylesheet" href="bootstrap-4.1.3-dist/css/bootstrap.min.css">
+	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="css/fixed.css">
+</head>
+
+<body data-spy="scroll" data-target="#navbarResponsive">
+
+<div id="home">
+<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+	<a class="navbar-brand" href="#"><img src="img/pizza.png"></a>
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
+		<span class="navbar-toggler-icon"></span>
+	</button>
+
+	<div class="collapse navbar-collapse" id="navbarResponsive">
+		<ul class="navbar-nav ml-auto">
+			<li class="nav-item">
+				<a class="nav-link" href="#home">Home</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="#course">Course</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="#features">Features</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="#resources">Resources</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="#clients">Clients</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="#contact">Contact</a>
+			</li>
+		</ul>
+	</div>
+</nav>
+<div class="landing">
+	<div class="home-wrap">
+		<div class="home-inner">
+		</div>
+	</div>
+</div>
+
+<div class="caption text-center">
+	<h1>Welcome to Pizza</h1>
+	<h3>Pizza pasta</h3>
+	<a class="btn btn-outline-light btn-lg" href="#course">Get started</a>
+</div>
+<!-------------------------------->
+</div>
+<!-------------------------------->
+<div id="course" class="offset">
+	<div class="col-12 narrow text-center">
+		<h1>Pizza time advanced course</h1>
+		<p class="lead">Learn advanced pizza making techniques</p>
+		<a class="btn btn-secondary bt-md" href="https://www.youtube.com/watch?v=nXO2T9rXGEI" target="_blank">Pizza time course</a>
+	</div> 
+</div>
+<div class="flexbox-container">
+	<div class="video"><iframe width="250" height="250" src="https://www.youtube.com/embed/sv3TXMSv6Lw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+	<div class="video"><iframe width="250" height="250" src="https://www.youtube.com/embed/lpvT-Fciu-4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+	<div class="video"><iframe width="250" height="250" src="https://www.youtube.com/embed/Jy3uREg7v_o" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+</div>
+<!-------------------------------->
+<div id="features" class="offset">
+<div class="jumbotron">
+<div class="narrow text-center">
+	<div class="col-12">
+		<h3 class="heading">Features</h3>
+		<div class="heading-underline"></div>
+	</div>
+	<div class="row text-center">
+		<div class="col-md-4">
+			<div class="feature">
+				<i class="fas fa-play-circle fa-4x" data-fa-transform="shrink-3 up-5"></i>
+				<h3>Pizza video's</h3>
+				<p>The best video's for making the best italian pizza.</p>
+			</div>
+		</div>
+		<div class="col-md-4">
+			<div class="feature">
+				<i class="fas fa-sliders-h fa-4x" data-fa-transform="shrink-4.5 up-4.5"></i>
+				<h3>Get the most out of it</h3>
+				<p>With custom sliders you will be able to create your own unique pizza.</p>
+			</div>
+		</div>
+		<div class="col-md-4">
+			<div class="feature">
+				<i class="fab fa-wpforms fa-4x" data-fa-transform="shrink-4 up-5"></i>
+				<h3>Contact forms</h3>
+				<p>You will always be able to contact us for help.</p>
+			</div>
+		</div>
+	</div>
+</div>
+</div>
+<div>
+	<table class="content-table">
+		<thead>
+			<tr>
+				<th></th>
+				<th></th>
+				<th></th>
+</div>
+</div>
+<!-------------------------------->
+<div id="resources" class="offset">
+	
+
+<div class="fixed-background">
+<div class="row dark text-center">
+<div class="col-12">
+		<h3 class="heading">Made with care</h3>
+		<div class="heading-underline"></div>
+	</div>
+	<div class="col-md-4">
+			<div class="feature">
+				<h3>Pizza</h3>
+				<p class="lead">Made out of the best flour.</p>
+			</div>
+		</div>
+		<div class="col-md-4">
+			<div class="feature">
+				<h3>Pasta</h3>
+				<p class="lead">Made out of the best flour.</p>
+			</div>
+		</div>
+		<div class="col-md-4">
+			<div class="feature">
+				<h3>Spaghetti</h3>
+				<p class="lead">Made out of the best flour.</p>
+			</div>
+		</div>
+</div>
+	<div class="fixed-wrap">
+		<div class="fixed">
+		</div>
+	</div>
+</div>
+
+</div>
+
+</div>
+<!-------------------------------->
+<div id="clients" class="offset">
+<div class="jumbotron">
+	<div class="col-12 text-center">
+		<h3 class="heading">Clients</h3>
+		<div class="heading-underline"></div>
+	</div>
+
+	<div class="row">
+		<div class="col-md-6 clients">
+		<div class="row">
+			<div class="col-md-4">
+				<img src="img/client1.png">
+			</div>
+			<div class="col-md-8">
+				<blockquote>
+					<i class="fas fa-thumbs-up"></i>
+					Mamma mia the best pizza i have ever had!
+					<hr class="clients-hr">
+					<cite>&#8212;Some random customer</cite>
+				</blockquote>
+			</div>
+		</div>
+		</div>
+
+		<div class="col-md-6 clients">
+		<div class="row">
+			<div class="col-md-4">
+				<img src="img/client2.png">
+			</div>
+			<div class="col-md-8">
+				<blockquote>
+					<i class="fas fa-thumbs-up"></i>
+					Mia mamma the best pizza i have ever had!
+					<hr class="clients-hr">
+					<cite>&#8212;Another random customer</cite>
+				</blockquote>
+			</div>
+		</div>
+		</div>
+	</div>
+</div>
+
+<div class="col-12 narrow text-center"> 
+<p class="lead">Want to learn more about italian kitchen?</p>
+<a class="btn btn-secondary bt-md" href="https://en.wikipedia.org/wiki/Italian_cuisine" target="_blank">Italian kitchen course</a>
+</div>
+
+</div>
+<!-------------------------------->
+<div id="contact" class="offset">
+
+<footer>
+<div class="row justify-content-center">
+	
+	<div class="col-md-5 text-center">
+		<img src="img/pizza.png">
+		<br>
+		<form action="/action_page.php" >
+			<label for="name">Name:</label>
+			<input type="text" class ="right"  id="name" name="name"><br><br>
+			<label for="surname">Surname:</label>
+			<input type="text" class ="right"  id="surname" name="surname"><br><br>
+			<label for="email">Email:</label>
+			<input type="email" class ="right"  id="email" name="email"><br><br>
+			<label for="Password">Password:</label>
+			<input type="password" class ="right" id="Password" name="Password"><br><br>
+			<input type="submit" value="Submit">
+		  </form>
+		<p>At our core is a collection of designs and food development solutions that represent everything your kitchen needs.</p>
+		<strong>Contact info</strong>
+		<p>(888)888-8888<br>email@pizza.com</p>
+		<a href="https://www.facebook.com/" target="_blank"><i class="fab fa-facebook-square"></i></a>
+		<a href="https://twitter.com/" target="_blank"><i class="fab fa-twitter-square"></i></a>
+		<a href="https://www.instagram.com/" target="_blank"><i class="fab fa-instagram"></i></a>
+		<div class="text-center">
+			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d139178.8864220671!2d23.989079130519666!3d56.9715833349355!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46eecfb0e5073ded%3A0x400cfcd68f2fe30!2zUsSrZ2E!5e0!3m2!1slv!2slv!4v1575916544536!5m2!1slv!2slv" width="450" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+		</div>
+	</div>
+	<hr class="socket">
+	&copy;Pizza Theme.
+
+</div>
+</footer>
+	
+</div>
+
+<!--- Script Source Files -->
+<script src="js/jquery-3.3.1.min.js"></script>
+<script src="bootstrap-4.1.3-dist/js/bootstrap.min.js"></script>
+<script src="https://use.fontawesome.com/releases/v5.5.0/js/all.js"></script>
+<!--- End of Script Source Files -->
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
